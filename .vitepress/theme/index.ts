aba0755261a4c394c493ff7e29d5195f4963a608
YShenZe <administer@mengze.vip>
2025-08-30 07:29:41 +0800

    add 'ApngPlayer' components

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,20 +1,17 @@
-// https://vitepress.dev/guide/custom-theme
 import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
-import Download from './components/Download.vue'
-import DownloadZL2 from './components/DownloadZL2.vue'
+import ApngPlayer from './components/ApngPlayer.vue'
 import './style.css'
 import './style/blur.css'
 
 export default {
   extends: DefaultTheme,
   Layout: () => {
-    return h(DefaultTheme.Layout, null, {
-      // https://vitepress.dev/guide/extending-default-theme#layout-slots
-    })
-  },
+    return h(DefaultTheme.Layout, null, {})
+    },
   enhanceApp({ app, router, siteData }) {
+    app.component('ApngPlayer', ApngPlayer)
     app.component('Download', Download)
     app.component('DownloadZL2', DownloadZL2)
   }
